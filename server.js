024f6dd87b5e946dc3dbd833cd198b8ff7b63d88
3116005181 <1470064691@qq.com>
2018-09-25 20:58:13 +0800

    原生异步请求

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 // const cors = require('cors')
 
+app.use(express.static('test'))
+
 app.listen(8080, function(){
   console.log('server start')
+})
+
+app.get('/home', (req,res)=>{
+  res.redirect('./XHR请求/XHR.html')
 })
 
 app.get('/api/jsonp', function(req,res){
@@ -17,5 +23,18 @@ app.get('/api/jsonp', function(req,res){
   res.jsonp({ // 请求成功
     code: true,
     mes: content
+  })
+})
+
+app.get('/api/xhr', (req, res)=>{
+  var { 
+    name
+  } = req.query
+
+  console.log(name)
+
+  res.json({
+    code: true,
+    mes: 'xhr'
   })
 })
